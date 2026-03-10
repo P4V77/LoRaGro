23696e607b4736adf593a8a9192ed094a3e821b1
P4V77 <pavelmich.id@gmail.com>
2026-03-10 17:51:30 +0100

    tests: add sensor tests for native_sim + docs update
    Add ztest unit tests for battery_sense, soil_capacitive_adapter
    and co2_sensor_adapter. All 29/29 test cases passing.
    
    - native_sim.overlay: adc-emul, i2c-emul, fake SCD41, storage chosen
    - prj.conf: enable SENSOR, I2C, NVS, FLASH, fake drivers
    - CMakeLists: register common/ as EXTRA_ZEPHYR_MODULES
    - common/Kconfig: sensor fake drivers default n

--- FW-LoRaGro/twister-out/native_sim_native/host/power_rail_3v3/power_rail_3v3.basic/zephyr/NSI/common/src/nsi_trace_varg.d
+++
@@ -1,6 +0,0 @@
-/home/pavel/Projects/LoRaGro/FW-LoRaGro/twister-out/native_sim_native/host/power_rail_3v3/power_rail_3v3.basic/zephyr/NSI/common/src/nsi_trace_varg.o: \
- /home/pavel/.zephyr_ide/external/zephyr/scripts/native_simulator//common/src/nsi_trace_varg.c \
- /home/pavel/.zephyr_ide/external/zephyr/scripts/native_simulator/common/src/include/nsi_tracing.h \
- /home/pavel/.zephyr_ide/external/zephyr/scripts/native_simulator/common/src/include/nsi_utils.h
-/home/pavel/.zephyr_ide/external/zephyr/scripts/native_simulator/common/src/include/nsi_tracing.h:
-/home/pavel/.zephyr_ide/external/zephyr/scripts/native_simulator/common/src/include/nsi_utils.h:
--- FW-LoRaGro/twister-out/native_sim_native/host/sensors/co2_sensor_adapter.basic/zephyr/NSI/linker_script.d
+++
@@ -1,2 +0,0 @@
-linker_script.pre.o: \
- /home/pavel/.zephyr_ide/external/zephyr/scripts/native_simulator//common/other/linker_script.pre.ld
